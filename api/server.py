680bbb239e39d434a217e67e8254ddb2e8c217ae
hugomdmg <hugomdmg@gmail.com>
2025-01-12 21:15:02 +0000

    vercel

--- api/server.py
+++ api/server.py
@@ -1,21 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/', methods=['GET'])
 def hello():
     return jsonify({'message': 'hello world'})
 
-
 @app.route('/api/post', methods=['POST'])
 def chat():
     user_input = request.json.get('message')
-    return jsonify({'message': user_input+' -> api'})
+    return jsonify({'message': user_input + ' -> api'})
 
 
+def handler(request):
+    with app.app_context():
+        return app.full_dispatch_request()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000) 
