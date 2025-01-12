6affe3943e443d6473acf7e1023f2b23553eb1a3
hugomdmg <hugomdmg@gmail.com>
2025-01-12 20:57:44 +0000

    fist commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/get', methods=['GET'])
+def hello():
+    return jsonify({'message': 'hello world'})
+
+
+@app.route('/api/post', methods=['POST'])
+def chat():
+    user_input = request.json.get('message')
+    return jsonify({'message': user_input+' -> api'})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
